ed995b315b7dc5d4779dbdef6e1b4b834aaeea1f
Vinicin735x <vini08.mantovani@gmail.com>
2022-09-01 17:42:21 -0300

    parei na pag 9

--- app/Models/Anime.ts
+++ app/Models/Anime.ts
@@ -1,13 +1,23 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Anime extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public autor: string
+
+  @column()
+  public data_lancamento: Date
+
+  @column()
+  public nome: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
 }
